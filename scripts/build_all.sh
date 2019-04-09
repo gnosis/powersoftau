e8ab4871d9506b95843441d1affce570dc944801
Giacomo Licari <giacomo.licari@gmail.com>
2019-04-09 11:56:12 +0200

    Comment not currently available libs

--- scripts/build_all.sh
+++ scripts/build_all.sh
@@ -1,8 +1,9 @@
 cargo build --release --bin new_constrained
 cargo build --release --bin new
 cargo build --release --bin compute_constrained
-cargo build --release --bin compute # Raises error: no bin target named `compute`
-cargo build --release --bin beacon # Raises error: no bin target named `beacon`
-cargo build --release --bin verify_transform # Raises error: no bin target named `verify_transform`
 cargo build --release --bin beacon_constrained
 cargo build --release --bin verify_transform_constrained
+
+# cargo build --release --bin compute # Not implemented yet
+# cargo build --release --bin beacon # Not implemented yet
+# cargo build --release --bin verify_transform # Not implemented yet
