0f0b8b45edcb5708f6a798a3648337cd5f95fb8b
josojo <alex@gnosis.pm>
2019-04-12 09:41:20 +0200

    Update build_all.sh

--- scripts/build_all.sh
+++ scripts/build_all.sh
@@ -1,9 +1,4 @@
 cargo build --release --bin new_constrained
-cargo build --release --bin new
 cargo build --release --bin compute_constrained
 cargo build --release --bin beacon_constrained
 cargo build --release --bin verify_transform_constrained
-
-# cargo build --release --bin compute # Not implemented yet
-# cargo build --release --bin beacon # Not implemented yet
-# cargo build --release --bin verify_transform # Not implemented yet
