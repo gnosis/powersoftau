893fc79f93cdcf36e2c2816f9a3529b76e929f8a
josojo <josojo@hotmail.de>
2019-04-22 22:51:38 +0200

    changing the file for reading env-variables

--- scripts/load_env_sshkey.sh
+++ scripts/load_env_sshkey.sh
@@ -2,13 +2,13 @@
 #
 # run the varariables.sh to load all enviromental variables set sets common aliases
 
-. /app/variables.sh
+. /tmp/volume/variables.sh
 
 # If `SSH_FILE_PATH` is empty/unset
 if [ -z "$SSH_FILE_PATH" ]
 then
-  # Set default SSH_FILE_PATH
-  SSH_FILE_PATH=/root/.ssh/id_rsa_worker
+    # Set default SSH_FILE_PATH
+    SSH_FILE_PATH=/root/.ssh/id_rsa_worker
 fi
 
 chmod 600 $SSH_FILE_PATH
