57be70769ad48b276e1adfd86a83afa39121b626
josojo <josojo@hotmail.de>
2019-04-24 11:06:25 +0200

    changing formating only

--- scripts/last_beacon_transition.sh
+++ scripts/last_beacon_transition.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-set -e 
+set -e
 ##Procedure -last setup:
 
-#Server runs the beacon step with tbd beacon input. 
+#Server runs the beacon step with tbd beacon input.
 if [[ ! -z "${CONSTRAINED}" ]]; then
-	cargo run --release --bin beacon_constrained
-	cargo run --release --bin verify_transform_constrained
+    cargo run --release --bin beacon_constrained
+    cargo run --release --bin verify_transform_constrained
 else
-	cargo run --release --bin beacon
-	cargo run --release --bin verify_transform
+    cargo run --release --bin beacon
+    cargo run --release --bin verify_transform
 fi
 
