75e6f8c15f989cb14973926d7bfeaed712ca14bc
josojo <josojo@hotmail.de>
2019-04-30 12:41:36 +0200

    formatting msg differently

--- scripts/send_msg_to_gitter.sh
+++ scripts/send_msg_to_gitter.sh
@@ -5,4 +5,4 @@
 
 . /app/scripts/load_env_sshkey.sh
 
- curl -X post -H "Content-Type: application/json" -H "Authorization: Bearer $GITTER_ACCESS_TOKEN" -d '{"text":"$1"}' "https://api.gitter.im/v1/rooms/$GITTER_ROOM/chatMessages"
+ curl -X post -H "Content-Type: application/json" -H "Authorization: Bearer $GITTER_ACCESS_TOKEN" -d "{\"text\":\"$1\"}" "https://api.gitter.im/v1/rooms/$GITTER_ROOM/chatMessages"
