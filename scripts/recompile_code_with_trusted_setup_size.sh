b0eebe88d846799e345c08173dd149ca059cde71
josojo <josojo@hotmail.de>
2019-05-09 11:15:46 +0200

    enabling easier testing

---
+++ scripts/recompile_code_with_trusted_setup_size.sh
@@ -0,0 +1,6 @@
+
+SUBSITUTION_COMMAND='s/const REQUIRED_POWER: usize = [0-9][0-9];*/const REQUIRED_POWER: usize ='$1';/g'
+echo "$SUBSITUTION_COMMAND"
+sed -i  "$SUBSITUTION_COMMAND" /app/src/bn256/mod.rs
+sed -i "$SUBSITUTION_COMMAND" /app/src/small_bn256/mod.rs
+. /app/scripts/build_all.sh
